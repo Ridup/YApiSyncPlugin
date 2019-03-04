4b14b2eb4bca281d2ff46c44202b36f6c985305e
Ridup <mu_ran@yahoo.com>
2019-03-04 18:21:00 +0800

    修改

--- README.md
+++ README.md
@@ -1,11 +1,47 @@
 YApiSyncPlugin
 
+# YApi 接口上传插件使用说明
+
 ## 写在前面
 
-## 快速使用
-aaa
-aaa
-aaa
-aaa
-aaa
+此插件主要用于工程代码同步至 YApi 接口平台，以便查看接口状态和信息。
+上传接口时，请自觉维护好接口列表
+- [x] 修复路径中包含常量所导致的异常情况
+- [x] 增加对模块路径的个性化中文名称匹配
+- [x] 对枚举等对象进行格式化展示
+- [ ] 支持批量 Api 接口上传
+- [ ] 简化配置流程
+
+## 使用方法
+
+1. 下载`YApiSyncPlugin.jar`,打开 idea preferneces->plugins-> install plugin from disk,导入 jar 包后(install)，重启
+2. 先登录 YApi 平台，使用域账号登陆
+3. 配置信息  
+   配置信息位置, 在项目目录下，.idea 文件夹下，找到 misc.xml (如果找不到.idea 请查看是否被折叠或被隐藏) 如果是 .ipr 模式创建的 就找到 项目名.ipr
+
+```xml
+
+  <component name="yapi">
+    <option name="projectToken">用户项目的Token</option>
+    <option name="projectId">用户项目ID</option>
+    <option name="yapiUrl">http://127.0.0.1:3000</option>
+    <option name="projectType">api</option>
+  </component>
+
+  <!-- 中台管理台接口平台配置示例 -->
+  <component name="yapi">
+    <option name="projectToken">4a948bd3ea80a842d30b5f3d8ee855615dc177924d495e441a0802d1cff9b02f</option>
+    <option name="projectId">50</option>
+    <option name="yapiUrl">http://127.0.0.1:3000</option>
+    <option name="projectType">api</option>
+  </component>
+```
+4. 上传
+
+- 如果是 dubbo 项目，选中 dubbo interface 文件中的一个方法（要<b>选中</b>方法名称），右击 YApiSyncPlugin(alt+u 快捷键)
+- 如果是 api 项目，选中 controller 类中的方法名称或类名（要<b>选中</b>方法名称，或类名，选中类名为当前类所有接口都上传），右击 YApiSyncPlugin(alt+u 快捷键)
+
+5. 选中
+
+- <b>双击</b>方法或类名即为选中,如果未选择任何地方 上传 默认按类(`Idea未分类接口`)上传
 aaa
---
+++ src/com/ridup/interaction/YApiSyncPlugin.java
@@ -0,0 +1,290 @@
+package com.ridup.interaction;
+
+import com.google.common.base.Strings;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.ridup.build.BuildJsonForComponent;
+import com.ridup.build.BuildJsonForDubbo;
+import com.ridup.build.BuildJsonForIntf;
+import com.ridup.build.BuildJsonForYapi;
+import com.ridup.constant.ProjectTypeConstant;
+import com.ridup.constant.YapiConstant;
+import com.ridup.dto.YapiApiDTO;
+import com.ridup.dto.YapiDubboDTO;
+import com.ridup.dto.YapiResponse;
+import com.ridup.dto.YapiSaveParam;
+import com.ridup.upload.YApiSync;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @description: 入口
+ * @author: ridup
+ * @date: 2019/5/15
+ */
+public class YApiSyncPlugin extends AnAction {
+
+    private static NotificationGroup notificationGroup;
+
+    static {
+        notificationGroup = new NotificationGroup("Java2Json.NotificationGroup", NotificationDisplayType.BALLOON, true);
+    }
+
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Editor editor = (Editor) e.getDataContext().getData(CommonDataKeys.EDITOR);
+        Project project = editor.getProject();
+        String projectToken = null;
+        String projectId = null;
+        String yapiUrl = null;
+        String projectType = null;
+        String returnClass = null;
+        String attachUpload = null;
+
+        // 添加开始提示
+        Notification notification = notificationGroup.createNotification("Uploading，please wait a moment" , NotificationType.INFORMATION);
+        Notifications.Bus.notify(notification, project);
+
+        // 获取配置
+        try {
+            String projectConfig = new String(editor.getProject().getProjectFile().contentsToByteArray(), "utf-8");
+            String[] modules = projectConfig.split("moduleList\">");
+            if (modules.length > 1) {
+                String[] moduleList = modules[1].split("</")[0].split(",");
+                PsiFile psiFile = (PsiFile) e.getDataContext().getData(CommonDataKeys.PSI_FILE);
+                String virtualFile = psiFile.getVirtualFile().getPath();
+                for (int i = 0; i < moduleList.length; i++) {
+                    if (virtualFile.equals(moduleList[i])) {
+                        projectToken = projectConfig.split(moduleList[i] + "\\.projectToken\">")[1].split("</")[0];
+                        projectId = projectConfig.split(moduleList[i] + "\\.projectId\">")[1].split("</")[0];
+                        yapiUrl = projectConfig.split(moduleList[i] + "\\.yapiUrl\">")[1].split("</")[0];
+                        projectType = projectConfig.split(moduleList[i] + "\\.projectType\">")[1].split("</")[0];
+                        if (projectConfig.split(moduleList[i] + "\\.returnClass\">").length > 1) {
+                            returnClass = projectConfig.split(moduleList[i] + "\\.returnClass\">")[1].split("</")[0];
+                        }
+                        String[] attachs = projectConfig.split(moduleList[i] + "\\.attachUploadUrl\">");
+                        if (attachs.length > 1) {
+                            attachUpload = attachs[1].split("</")[0];
+                        }
+                        break;
+                    }
+                }
+            } else {
+                projectToken = projectConfig.split("projectToken\">")[1].split("</")[0];
+                projectId = projectConfig.split("projectId\">")[1].split("</")[0];
+                yapiUrl = projectConfig.split("yapiUrl\">")[1].split("</")[0];
+                projectType = projectConfig.split("projectType\">")[1].split("</")[0];
+                if (projectConfig.split("returnClass\">").length > 1) {
+                    returnClass = projectConfig.split("returnClass\">")[1].split("</")[0];
+                }
+
+                String[] attachs = projectConfig.split("attachUploadUrl\">");
+                if (attachs.length > 1) {
+                    attachUpload = attachs[1].split("</")[0];
+                }
+            }
+        } catch (Exception e2) {
+            Notification error = notificationGroup.createNotification("get config error:" + e2.getMessage(), NotificationType.ERROR);
+            Notifications.Bus.notify(error, project);
+            return;
+        }
+        // 配置校验
+        if (Strings.isNullOrEmpty(projectToken) || Strings.isNullOrEmpty(projectId) || Strings.isNullOrEmpty(yapiUrl) || Strings.isNullOrEmpty(projectType)) {
+            Notification error = notificationGroup.createNotification("please check config,[projectToken,projectId,yapiUrl,projectType]", NotificationType.ERROR);
+            Notifications.Bus.notify(error, project);
+            return;
+        }
+        // 判断项目类型
+        if (ProjectTypeConstant.dubbo.equals(projectType)) {
+            // 获得dubbo需上传的接口列表 参数对象
+            ArrayList<YapiDubboDTO> yapiDubboDTOs = new BuildJsonForDubbo().actionPerformedList(e);
+            if (yapiDubboDTOs != null) {
+                for (YapiDubboDTO yapiDubboDTO : yapiDubboDTOs) {
+                    YapiSaveParam yapiSaveParam = new YapiSaveParam(projectToken, yapiDubboDTO.getTitle(), yapiDubboDTO.getPath(), yapiDubboDTO.getParams(), yapiDubboDTO.getResponse(), Integer.valueOf(projectId), yapiUrl, yapiDubboDTO.getDesc());
+                    yapiSaveParam.setStatus(yapiDubboDTO.getStatus());
+                    if (!Strings.isNullOrEmpty(yapiDubboDTO.getMenu())) {
+                        yapiSaveParam.setMenu(yapiDubboDTO.getMenu());
+                    } else {
+                        yapiSaveParam.setMenu(YapiConstant.menu);
+                    }
+                    try {
+                        // 上传
+                        YapiResponse yapiResponse = new YApiSync().uploadSave(yapiSaveParam, null, project.getBasePath());
+                        if (yapiResponse.getErrcode() != 0) {
+                            Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + yapiResponse.getErrmsg(), NotificationType.ERROR);
+                            Notifications.Bus.notify(error, project);
+                        } else {
+                            String url = yapiUrl + "/project/" + projectId + "/interface/api/cat_" + yapiResponse.getCatId();
+                            this.setClipboard(url);
+                            Notification error = notificationGroup.createNotification("success ,url: " + url, NotificationType.INFORMATION);
+                            Notifications.Bus.notify(error, project);
+                        }
+                    } catch (Exception e1) {
+                        Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + e1, NotificationType.ERROR);
+                        Notifications.Bus.notify(error, project);
+                    }
+                }
+            }
+        } else if (ProjectTypeConstant.api.equals(projectType)) {
+            //获得api 需上传的接口列表 参数对象
+            ArrayList<YapiApiDTO> yapiApiDTOS = new BuildJsonForYapi().actionPerformedList(e, attachUpload, returnClass);
+            if (yapiApiDTOS != null) {
+                for (YapiApiDTO yapiApiDTO : yapiApiDTOS) {
+                    YapiSaveParam yapiSaveParam = new YapiSaveParam(projectToken, yapiApiDTO.getTitle(), yapiApiDTO.getPath(), yapiApiDTO.getParams(), yapiApiDTO.getRequestBody(), yapiApiDTO.getResponse(), Integer.valueOf(projectId), yapiUrl, true, yapiApiDTO.getMethod(), yapiApiDTO.getDesc(), yapiApiDTO.getHeader());
+                    yapiSaveParam.setReq_body_form(yapiApiDTO.getReq_body_form());
+                    yapiSaveParam.setReq_body_type(yapiApiDTO.getReq_body_type());
+                    yapiSaveParam.setReq_params(yapiApiDTO.getReq_params());
+                    yapiSaveParam.setStatus(yapiApiDTO.getStatus());
+                    if (!Strings.isNullOrEmpty(yapiApiDTO.getMenu())) {
+                        yapiSaveParam.setMenu(yapiApiDTO.getMenu());
+                    } else {
+                        yapiSaveParam.setMenu(YapiConstant.menu);
+                    }
+                    try {
+                        // 上传
+                        YapiResponse yapiResponse = new YApiSync().uploadSave(yapiSaveParam, attachUpload, project.getBasePath());
+                        if (yapiResponse.getErrcode() != 0) {
+                            Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + yapiResponse.getErrmsg(), NotificationType.ERROR);
+                            Notifications.Bus.notify(error, project);
+                        } else {
+                            String url = yapiUrl + "/project/" + projectId + "/interface/api/cat_" + yapiResponse.getCatId();
+                            this.setClipboard(url);
+                            Notification error = notificationGroup.createNotification("success ,url:  " + url, NotificationType.INFORMATION);
+                            Notifications.Bus.notify(error, project);
+                        }
+                    } catch (Exception e1) {
+                        Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + e1, NotificationType.ERROR);
+                        Notifications.Bus.notify(error, project);
+                    }
+                }
+            }
+        }
+        // Spring服务、接口等
+        else if(ProjectTypeConstant.component.equals(projectType)){
+            //获得组件需上传的接口列表 参数对象
+            ArrayList<YapiApiDTO> yapiApiDTOS = new BuildJsonForComponent().actionPerformedList(e, attachUpload, returnClass);
+            if (yapiApiDTOS != null) {
+                for (YapiApiDTO yapiApiDTO : yapiApiDTOS) {
+                    YapiSaveParam yapiSaveParam = new YapiSaveParam(projectToken, yapiApiDTO.getTitle(), yapiApiDTO.getPath(), yapiApiDTO.getParams(), yapiApiDTO.getRequestBody(),
+                        yapiApiDTO.getResponse(), Integer.valueOf(projectId), yapiUrl, true, yapiApiDTO.getMethod(),
+                        yapiApiDTO.getDesc(), yapiApiDTO.getHeader());
+                    yapiSaveParam.setReq_body_form(yapiApiDTO.getReq_body_form());
+                    yapiSaveParam.setReq_body_type(yapiApiDTO.getReq_body_type());
+                    yapiSaveParam.setReq_params(yapiApiDTO.getReq_params());
+                    yapiSaveParam.setStatus(yapiApiDTO.getStatus());
+                    if (!Strings.isNullOrEmpty(yapiApiDTO.getMenu())) {
+                        yapiSaveParam.setMenu(yapiApiDTO.getMenu());
+                    } else {
+                        yapiSaveParam.setMenu(YapiConstant.menu);
+                    }
+                    //路径为 /BeanName/MethodName
+                    if(StringUtils.isBlank(yapiApiDTO.getPath())){
+                        yapiSaveParam.setPath("/pending/"+ new Date().getTime());
+                    }else{
+                        yapiSaveParam.setPath(yapiApiDTO.getBasePath()+yapiApiDTO.getPath());
+                    }
+
+                    try {
+                        // 上传
+                        YapiResponse yapiResponse = new YApiSync().uploadSave(yapiSaveParam, attachUpload, project.getBasePath());
+                        if (yapiResponse.getErrcode() != 0) {
+                            Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + yapiResponse.getErrmsg(), NotificationType.ERROR);
+                            Notifications.Bus.notify(error, project);
+                        } else {
+                            String url = yapiUrl + "/project/" + projectId + "/interface/api/cat_" + yapiResponse.getCatId();
+                            this.setClipboard(url);
+                            Notification error = notificationGroup.createNotification("success ,url:  " + url,
+                                NotificationType.INFORMATION);
+                            Notifications.Bus.notify(error, project);
+                        }
+                    } catch (Exception e1) {
+                        Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + e1,
+                            NotificationType.ERROR);
+                        Notifications.Bus.notify(error, project);
+                    }
+                }
+                notification.expire();
+            }
+        }
+        // Spring服务、接口等
+        else if(ProjectTypeConstant.intf.equals(projectType)){
+            //获得组件需上传的接口列表 参数对象
+            ArrayList<YapiApiDTO> yapiApiDTOS = new BuildJsonForIntf().actionPerformedList(e, attachUpload, returnClass);
+            if (yapiApiDTOS != null) {
+                for (YapiApiDTO yapiApiDTO : yapiApiDTOS) {
+                    YapiSaveParam yapiSaveParam = new YapiSaveParam(projectToken, yapiApiDTO.getTitle(), yapiApiDTO.getPath(), yapiApiDTO.getParams(), yapiApiDTO.getRequestBody(),
+                        yapiApiDTO.getResponse(), Integer.valueOf(projectId), yapiUrl, true, yapiApiDTO.getMethod(),
+                        yapiApiDTO.getDesc(), yapiApiDTO.getHeader());
+                    yapiSaveParam.setReq_body_form(yapiApiDTO.getReq_body_form());
+                    yapiSaveParam.setReq_body_type(yapiApiDTO.getReq_body_type());
+                    yapiSaveParam.setReq_params(yapiApiDTO.getReq_params());
+                    yapiSaveParam.setStatus(yapiApiDTO.getStatus());
+                    if (!Strings.isNullOrEmpty(yapiApiDTO.getMenu())) {
+                        yapiSaveParam.setMenu(yapiApiDTO.getMenu());
+                    } else {
+                        yapiSaveParam.setMenu(YapiConstant.menu);
+                    }
+                    //路径为 /BeanName/MethodName
+                    if(StringUtils.isBlank(yapiApiDTO.getPath())){
+                        yapiSaveParam.setPath("/pending/"+ new Date().getTime());
+                    }else{
+                        yapiSaveParam.setPath(yapiApiDTO.getBasePath()+yapiApiDTO.getPath());
+                    }
+
+                    try {
+                        // 上传
+                        YapiResponse yapiResponse = new YApiSync().uploadSave(yapiSaveParam, attachUpload, project.getBasePath());
+                        if (yapiResponse.getErrcode() != 0) {
+                            Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + yapiResponse.getErrmsg(), NotificationType.ERROR);
+                            Notifications.Bus.notify(error, project);
+                        } else {
+                            String url = yapiUrl + "/project/" + projectId + "/interface/api/cat_" + yapiResponse.getCatId();
+                            this.setClipboard(url);
+                            Notification error = notificationGroup.createNotification("success ,url:  " + url,
+                                NotificationType.INFORMATION);
+                            Notifications.Bus.notify(error, project);
+                        }
+                    } catch (Exception e1) {
+                        Notification error = notificationGroup.createNotification("sorry ,upload api error cause:" + e1,
+                            NotificationType.ERROR);
+                        Notifications.Bus.notify(error, project);
+                    }
+                }
+                notification.expire();
+            }
+        }
+
+    }
+
+    /**
+     * @description: 设置到剪切板
+     * @param: [content]
+     * @return: void
+     * @author: ridup
+     * @date: 2019/7/3
+     */
+    private void setClipboard(String content) {
+        //获取系统剪切板
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        //构建String数据类型
+        StringSelection selection = new StringSelection(content);
+        //添加文本到系统剪切板
+        clipboard.setContents(selection, null);
+    }
+}
